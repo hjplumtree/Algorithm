675d30363f184f58ce843a7641cea8680fe4ac87
Hee Jae <dark199174@gmail.com>
2021-09-22 22:29:34 +0900

    base case trimmed

--- 7. Stairs/stairs.js
+++ 7. Stairs/stairs.js
@@ -2,12 +2,9 @@ let fs = require('fs')
 const input = +fs.readFileSync('stairs.txt')
 
 function stairs(n) {
-    if(n === 1) {
+    if(n <= 1) {
         return 1
     }
-    else if(n === 2) {
-        return 1 + stairs(1)
-    } 
     else {
         return stairs(n - 1) + stairs(n - 2)
     }
