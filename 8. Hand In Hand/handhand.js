20b097e79c512b76b14ba07977ca17a71a1f9122
Hee Jae <dark199174@gmail.com>
2021-09-24 21:23:23 +0900

    handhand done room to improve

--- 8. Hand In Hand/handhand.js
+++ 8. Hand In Hand/handhand.js
@@ -3,9 +3,34 @@ let input = fs.readFileSync('handhand.txt').toString().split('\n')
 
 const ppl = +input.shift()
 let coorArr = []
-
+let sameLine = 0
+let tempDist = 0
+let distance = 0
 coorArr = input.sort((a, b) => {
-    return a[1] - b[1]
+    if(a[2] === b[2]) {
+        return a[0] - b[0]
+    }
+    else {
+        return a[2] - b[2]
+    }
 })
 
-console.log(coorArr);
+function showDist(coor) {
+    for(let i = 0; i < coor.length-1; i++) {
+        if(sameLine === 0 && coor[i][2] !== coor[i+1][2]) {
+            return 'FAIL'
+        }
+        if(coor[i][2] === coor[i+1][2]) {
+            sameLine = 1
+            tempDist = +coor[i+1][0] - +coor[i][0]
+            distance = Math.max(+distance, +tempDist)
+        }
+        else {
+            sameLine = 0
+            continue;
+        }
+    }
+    return `SUCCESS \n${distance}`
+}
+
+console.log(showDist(coorArr));
