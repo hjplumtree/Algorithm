8081fb3a2e9beda2b80c6237830462b2f49f93e5
Hee Jae <dark199174@gmail.com>
2021-09-25 22:59:13 +0900

    merge sort made, answer to make

--- 11. Marathon/marathon.js
+++ 11. Marathon/marathon.js
@@ -3,33 +3,31 @@ let input = fs.readFileSync('marathon.txt').toString().split(' ')
 
 function mergeSort(arr) {
     if(arr.length < 2) {
-        return arr
+        return arr.map(Number)
     }
     let mid = Math.floor(arr.length / 2)
     
     let a_l = arr.slice(0, mid)
     let a_r = arr.slice(mid)
-    merge(mergeSort(a_l), mergeSort(a_r))
+    return merge(mergeSort(a_l), mergeSort(a_r))
 }
 
 function merge(a_l, a_r) {
     let result = []
     let left_index = 0
     let right_index = 0
-    console.log(a_r);
     while(left_index < a_l.length && right_index < a_r.length) {
-        console.log(right_index);
+        console.log('yo',right_index);
         if(a_l[left_index] <= a_r[right_index]) {
-            result.push(+a_l[left_index])
+            result.push(a_l[left_index])
             left_index++
         }
         else {
-            result.push(+a_r[right_index])
-            console.log(result);
+            result.push(a_r[right_index])
             right_index++
         }
     }   
     return result.concat(a_l.slice(left_index), a_r.slice(right_index))
 }
 
-console.log(mergeSort(input));
+console.log(mergeSort(input))
